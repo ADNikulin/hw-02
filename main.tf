06449b0c484c0893c23b56369ccbc7bd6be61b04
anikulin <sanches00790@gmail.com>
2024-07-28 21:08:43 +0300

    solution 2

--- main.tf
+++ main.tf
@@ -11,17 +11,17 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_family_os_name
 }
 
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v3"
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_settings.platform_id
   
   resources {
-    cores         = 2
-    memory        = 2
-    core_fraction = 20
+    cores         = var.vm_web_platform_settings.core_count
+    memory        = var.vm_web_platform_settings.memory_count
+    core_fraction = var.vm_web_platform_settings.core_fraction
   }
 
   boot_disk {
@@ -31,7 +31,7 @@ resource "yandex_compute_instance" "platform" {
   }
 
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_platform_settings.preemptible
   }
 
   network_interface {
@@ -41,7 +41,7 @@ resource "yandex_compute_instance" "platform" {
 
   metadata = {
     serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+    ssh-keys           = "${var.vms_ssh_user}:${var.vms_ssh_root_key}"
   }
 
 }
--- variables.tf
+++ variables.tf
@@ -55,3 +55,40 @@ variable "vms_ssh_root_key" {
   default     = "<your_ssh_ed25519_key>"
   description = "ssh-keygen -t ed25519"
 }
+
+variable "vms_ssh_user" {
+  type        = string
+  default     = "ubuntu"
+  description = "ssh <vms_ssh_user>@any-ip"
+}
+
+### vm settings 
+variable "vm_web_family_os_name" {
+  type = string
+  default = "ubuntu-2004-lts"
+  description = "family os name"
+}
+
+variable "vm_web_name" {
+  type = string
+  default = "netology-develop-platform-web"
+  description = "vm name name"
+}
+
+variable "vm_web_platform_settings" {
+  type = object({
+    core_count = number
+    memory_count = number
+    core_fraction = number
+    preemptible = bool
+    platform_id = string
+  })
+  default = {
+    core_count = 2
+    core_fraction = 20
+    memory_count = 2
+    preemptible = true
+    platform_id = "standard-v3"
+  }
+  description = "https://yandex.cloud/ru/docs/compute/concepts/vm-platforms"
+}
